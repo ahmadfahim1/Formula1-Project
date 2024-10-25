c73d5b0a81d31580987c7326b7da7d7c4d7959ef
Emaz Uddin Ahmad <47092860+ahmadfahim1@users.noreply.github.com>
2024-10-25 22:07:11 +0300

    Add files via upload

---
+++ Includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/formula7dl7/raw'
+processed_folder_path = '/mnt/formula7dl7/processed'
+presentation_folder_path = '/mnt/formula7dl7/presentation'
+
+# COMMAND ----------
+
